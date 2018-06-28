63c2a9c998581c353edf3bf013146086dfb3b185
Magdalena Nowak <magdanowak89@gmail.com>
2018-06-28 23:03:38 +0200

    added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+with open('README.rst') as readme:
+    LONG_DESCRIPTION = readme.read()
+    
 setup(name='vroom',
       license='MIT',
-      version='1.0.0',
+      version='1.0.1',
       description='License Plate Parser',
+      long_description=LONG_DESCRIPTION,
       author='Magdalena Nowak',
       author_email='magdanowak0804@gmail.com',
       url='https://github.com/magdanowak/vroom',
