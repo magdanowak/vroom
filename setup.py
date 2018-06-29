54fae8ade141b5c6f73cdb6e3f8e03692ee00f49
Magdalena Nowak <magdanowak89@gmail.com>
2018-06-29 23:10:19 +0200

    correcting package data options

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as readme:
     
 setup(name='vroom',
       license='MIT',
-      version='1.0.1',
+      version='1.0.2',
       description='License Plate Parser',
       long_description=LONG_DESCRIPTION,
       author='Magdalena Nowak',
@@ -13,6 +13,5 @@ setup(name='vroom',
       url='https://github.com/magdanowak/vroom',
       install_requires=['pyyaml'],
       packages=['vroom'],
-      package_data={'vroom': ['*.yml']},
-      include_package_data=True
+      package_data={'vroom': ['*.yml']}
      )
